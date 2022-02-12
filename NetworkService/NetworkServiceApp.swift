fc958032c9abb0ceae09007b4d3a4a11bef55019
tuwiqBootcamp <abdullah.ali.ruh.s@tuwaiq.edu.sa>
2022-02-12 22:38:37 +0300

    Initial Commit

---
+++ NetworkService/NetworkServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceApp.swift
+//  NetworkService
+//
+//  Created by Abdullah Alnutayfi on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
